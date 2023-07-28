cb6dc6efcd6adb3804e36bc74a3d9bc8d453e264
Adeel <mohd.adeel.mughal@outlook.com>
2023-07-29 01:47:00 +0500

    half navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import './App.css';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
+      <h1>
+        Anim ut sunt ex commodo. Deserunt magna incididunt laboris ipsum culpa
+        dolor consequat velit ex veniam id. Magna irure consectetur commodo
+        magna elit exercitation quis aliquip cillum. Pariatur consectetur sit
+        duis labore fugiat irure ipsum duis quis nisi eu eu.
+      </h1>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+  const [active, setActive] = useState(true);
 
-export default Navbar
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  });
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="link">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>T1 </span>
+            <span>T2 </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
